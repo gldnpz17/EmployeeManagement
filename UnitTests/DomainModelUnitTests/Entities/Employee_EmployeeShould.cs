f6cee554946ee1c0cd6c523fb8856fb4744a0b94
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-04-27 21:56:47 +0700

    Finished domain model unit tests.

--- UnitTests/DomainModelUnitTests/Entities/Employee_EmployeeShould.cs
+++ UnitTests/DomainModelUnitTests/Entities/Employee_EmployeeShould.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DomainModel.Entities;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,21 @@ using System.Threading.Tasks;
 
 namespace DomainModelUnitTests.Entities
 {
+    [TestFixture]
     public class Employee_EmployeeShould
     {
-        
+        [Test]
+        public void Employee_InputIsConstructorArgs_ShouldProperlyInitialize()
+        {
+            var position = new Position()
+            {
+                Name = "TestPosition"
+            };
+
+            var employee = new Employee("TestName", position);
+
+            Assert.AreEqual(employee.Name, "TestName");
+            Assert.AreSame(employee.Position, position);
+        }
     }
 }
