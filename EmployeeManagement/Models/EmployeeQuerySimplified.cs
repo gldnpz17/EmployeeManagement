2d571f24bf55c3a18f857b3b32ac6d6855b8a158
gldnpz17 <firdausbismasuryakusuma@gmail.com>
2021-05-01 07:59:23 +0700

    Added position to EmployeeQuerySimplified.

--- EmployeeManagement/Models/EmployeeQuerySimplified.cs
+++ EmployeeManagement/Models/EmployeeQuerySimplified.cs
@@ -10,5 +10,6 @@ namespace EmployeeManagement.Models
     {
         public Guid EmployeeId { get; set; }
         public string Name { get; set; }
+        public string Position { get; set; }
     }
 }
